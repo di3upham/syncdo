225f0fd0780f5b31cb2aae478d07e78db120da64
di3upham <di3upham@gmail.com>
2021-06-16 21:10:13 +0700

    rm line, fix delete klock

--- README.md
+++ README.md
@@ -12,37 +12,14 @@ import (
 )
 
 func main() {
-	for i := 0; i < 100; i++ {
-		go func(index int) {
-			lnum, k := new(int), strconv.Itoa(index%10) // do not reuse
-			syncdo.Line(k, lnum, func() {
-				time.Sleep(100 * time.Millisecond)
-				fmt.Println(index)
-				syncdo.Line(k, lnum, func() {}) // nested
-			})
-		}(i)
-	}
+	unlock := syncdo.KLock("f1")
+	fmt.Println("hello world")
+	unlock()
+}
 
-	for i := 100; i < 200; i++ {
-		go func(index int) {
-			syncdo.Line(strconv.Itoa(index%5), new(int), func() {
-				time.Sleep(100 * time.Millisecond)
-				fmt.Println(index)
-			})
-		}(i)
-	}
-
-	for i := 200; i < 300; i++ {
-		go func(index int) {
-			unlock := syncdo.KLock(strconv.Itoa(index % 10))
-			time.Sleep(100 * time.Millisecond)
-			fmt.Println(index)
-			unlock()
-		}(i)
-	}
-
-	time.Sleep(1110 * time.Millisecond)
-	fmt.Println(len(klm), len(kls), cap(kls))
+func fx() {
+	defer syncdo.KLock("f1")()
+	// function body
 }
 
 ```
--- klock.go
+++ klock.go
@@ -1,6 +1,13 @@
 package syncdo
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+var ll = &sync.Mutex{}
+var klm = make(map[string]*kmutex)
+var kls = make([]*kmutex, 0)
 
 func KLock(k string) func() {
 	var kl *kmutex
@@ -17,15 +24,15 @@ func KLock(k string) func() {
 
 DO:
 	kl.num++
-	n := new(int)
-	*n = kl.num
 	ll.Unlock()
 	defer kl.Lock()
 	return func() {
 		ll.Lock()
-		if *n == kl.num {
+		kl.rnum++
+		if kl.rnum == kl.num {
 			kl.key = nil
 			kl.num = 0
+			kl.rnum = 0
 			delete(klm, k)
 		}
 		ll.Unlock()
@@ -35,8 +42,26 @@ DO:
 
 func Status() string {
 	ll.Lock()
-	runlen := len(klm)
-	llen := len(kls)
+	m := len(klm)
+	n := len(kls)
 	ll.Unlock()
-	return fmt.Sprintf("locked-len=%d lock-total=%d", runlen, llen)
+	return fmt.Sprintf("locking %d/%d", m, n)
+}
+
+func usekl() *kmutex {
+	for _, kl := range kls {
+		if kl.key == nil {
+			return kl
+		}
+	}
+	kl := &kmutex{Mutex: &sync.Mutex{}}
+	kls = append(kls, kl)
+	return kl
+}
+
+type kmutex struct {
+	*sync.Mutex
+	key  *string
+	num  int
+	rnum int
 }
